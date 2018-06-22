c922cf9f29f41f17566cbd1c993131318cd4190a
Casper Rogild Storm <casperstorm@me.com>
2018-06-22 16:14:04 +0200

    lint and gitignore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,14 @@
-import * as functions from 'firebase-functions';
-import * as express from 'express'
-import newsRouter from './routes/news'
+import * as functions from "firebase-functions";
+import * as express from "express";
+import newsRouter from "./routes/news";
 
-const PORT = process.env || 3000
+const PORT = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
 // new RequestHandlerParams
-app.use('/news', newsRouter)
+app.use("/news", newsRouter);
+console.log("hello");
 
-app.listen(PORT)
+app.listen(PORT);
 
 export const shine = functions.https.onRequest(app);
