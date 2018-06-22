83e54a4bd39fe7716bf089b7810ed84425ecb55e
Steffen Agger <steffen.agger@gmail.com>
2018-06-22 16:55:53 +0200

    missed conflict

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import * as functions from "firebase-functions";
-import * as express from "express";
-import newsRouter from "./routes/news";
-
-const PORT = process.env.PORT || 3000;
-
-const app = express();
-// new RequestHandlerParams
-app.use("/news", newsRouter);
-console.log("hello");
-=======
 import * as functions from 'firebase-functions'
 // import * as admin from 'firebase-admin'
 import * as express from 'express'
@@ -21,7 +9,6 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 app.use('/news', newsRouter)
->>>>>>> kinda working
 
 app.listen(PORT);
 
